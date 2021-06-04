bb670278b7faf82154d6256e6a283fa3e226c00b
Annabelle Nissl <annabelle.nissl@gmail.com>
2021-06-04 11:41:05 +0200

    tweaked values

--- src/simulation/utility.cpp
+++ src/simulation/utility.cpp
@@ -3,19 +3,19 @@
 
 using namespace LGen;
 
-double Utility::utility(const LRender::ReportAgent &report) const {
-	if(report.getExposure().getExposure() == 0) {
-		if(report.getSize().getNodes() > MAX_ZERO_SURFACE_SYMBOLS)
+double Utility::utility(const LRender::ReportAgent& report) const {
+	if (report.getExposure().getExposure() == 0) {
+		if (report.getSize().getNodes() > MAX_ZERO_SURFACE_SYMBOLS)
 			return -1;
 
 		return 0;
 	}
 
-	if(report.getLimits().getMinimum().y < report.getPosition().y)
+	if (report.getLimits().getMinimum().y < report.getPosition().y)
 		return -1;
 
 	return
-		(getLight(report) * 1.0) + (getNutrition(report) * 0.3) + (getArea(report) * 0.005);
+		pow(getLight(report), 0.5) * pow(getNutrition(report), 3.0) * pow(getArea(report), 5.0);
 }
 
 double Utility::getNutrition(const LRender::ReportAgent& report) const {
@@ -31,19 +31,19 @@ double Utility::getArea(const LRender::ReportAgent& report) const {
 	return report.getSize().getNodes();
 }
 
-double Utility::getFactorExposure(const LRender::ReportAgent &report) const {
+double Utility::getFactorExposure(const LRender::ReportAgent& report) const {
 	return std::pow(report.getExposure().getExposure(), 2.0f) / report.getSize().getNodes();
 }
 
-double Utility::getFactorSeeds(const LRender::ReportAgent &report) const {
+double Utility::getFactorSeeds(const LRender::ReportAgent& report) const {
 	return 1.0f / (report.getSeeds().size() * 0.05f + 1);
 }
 
-double Utility::getFactorRules(const LRender::ReportAgent &report) const {
+double Utility::getFactorRules(const LRender::ReportAgent& report) const {
 	return 1.0f / (report.getRules().getRuleCount() * 0.05f + 1);
 }
 
-double Utility::getFactorStability(const LRender::ReportAgent &report) const {
+double Utility::getFactorStability(const LRender::ReportAgent& report) const {
 	auto averageDelta = (report.getPosition() - report.getAverage());
 
 	const auto eccentricity = averageDelta.length();
@@ -51,10 +51,10 @@ double Utility::getFactorStability(const LRender::ReportAgent &report) const {
 	return 1.0f / (eccentricity * eccentricity * 0.3f + 1);
 }
 
-double Utility::getFactorLeaves(const LRender::ReportAgent &report) const {
+double Utility::getFactorLeaves(const LRender::ReportAgent& report) const {
 	float factorLeafArea = 0;
 
-	for(const auto &leaf : report.getLeaves()) {
+	for (const auto& leaf : report.getLeaves()) {
 		const auto areaFactor = leaf.getArea() * 8;
 
 		factorLeafArea += 1 - areaFactor * areaFactor;
